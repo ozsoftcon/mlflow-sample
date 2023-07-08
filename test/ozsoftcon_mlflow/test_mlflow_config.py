be186dd31857f107ea8ef06a5dcb4f71972c111f
Rajib Chakravorty <129637958+ozsoftcon@users.noreply.github.com>
2023-07-08 22:13:30 +1000

    Issues/9 (#21)
    * added data split method in the package
    
    * created run within an experiemnt
    
    * tested the new run the main app

--- test/ozsoftcon_mlflow/test_mlflow_config.py
+++ test/ozsoftcon_mlflow/test_mlflow_config.py
@@ -1,6 +1,7 @@
 from pytest import raises
 from unittest import mock, TestCase
 from ozsoftcon.mlflow_wrap import MLFlowConfig
+from ozsoftcon.mlflow_wrap import create_experiment
 from ozsoftcon.utils import InvalidMLFlowUri
 
 class TestMlFlowConfig(TestCase):
@@ -24,13 +25,60 @@ class TestMlFlowConfig(TestCase):
             mocked_tracking_uri_set
     ):
 
-        with mock.patch.dict(
-            'os.environ', {
-                    'MLFLOW_TRACKING_URI': 'valid-tracking-uri',
-                    'MLFLOW_REGISTRY_URI': 'valid-registry-uri'
-            }
-        ):
-            mlflow_config = MLFlowConfig()
-            assert mlflow_config.mlflow_client.tracking_uri == 'valid-tracking-uri'
-            assert mocked_tracking_uri_set.called_once_with('valid-tracking-uri')
+        mlflow_config = MLFlowConfig('valid-tracking-uri', 'valid-registry-uri')
+        assert mlflow_config.mlflow_client.tracking_uri == 'valid-tracking-uri'
+        assert mocked_tracking_uri_set.called_once_with('valid-tracking-uri')
+        assert mocked_registry_uri_set.called_once_with('valid-registry-uri')
 
+
+    def test_create_valid_run(
+            self
+    ):
+        mlflow_config = MLFlowConfig(
+            "valid-tracking-uri",
+            "valid-registry-uri"
+        )
+
+        experiment_id = create_experiment(
+            mlflow_config.mlflow_client, "test-experiment"
+        )
+
+        test_run = mlflow_config.create_run_for_experiment(
+            experiment_id, {"subject": "cifar-10"}, "test-run"
+        )
+
+        assert test_run.info.run_name == "test-run"
+        self.assertDictEqual(
+            test_run.data.tags, {
+                "mlflow.runName": "test-run",
+                "subject": "cifar-10"
+            })
+        assert test_run.info.experiment_id == experiment_id
+
+    @mock.patch('ozsoftcon.mlflow_wrap.mlflow_config.uuid4')
+    def test_create_run_with_no_name(
+            self, mocked_uuid4
+    ):
+        class MockedUUID4:
+            hex = "123456"
+
+        mocked_uuid4.return_value = MockedUUID4()
+
+        mlflow_config = MLFlowConfig(
+            "valid-tracking-uri",
+            "valid-registry-uri"
+        )
+
+        experiment_id = create_experiment(
+            mlflow_config.mlflow_client, "test-experiment")
+
+        test_run = mlflow_config.create_run_for_experiment(
+            experiment_id, {"subject": "cifar-10"}
+        )
+        assert test_run.info.run_name == "Run 123456 created for experiment"
+        self.assertDictEqual(
+            test_run.data.tags, {
+                "mlflow.runName": "Run 123456 created for experiment",
+                "subject": "cifar-10"
+            })
+        assert test_run.info.experiment_id == experiment_id
