0117ca0ab76b6f0f0628f6b5bd4adadd8e5878a4
RChakravorty <ozsoftcon@gmail.com>
2023-07-08 21:51:08 +1000

    created run within an experiemnt

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+from os import environ
 from dotenv import load_dotenv
 from ozsoftcon.mlflow_wrap import (
     MLFlowConfig, create_experiment
@@ -5,9 +6,13 @@ from ozsoftcon.mlflow_wrap import (
 
 load_dotenv()
 
+tracking_uri = environ.get("MLFLOW_TRACKING_URI", "")
+registry_uri = environ.get("MLFLOW_REGISTRY_URI", "")
+
+
 def main():
 
-    mlflow_config = MLFlowConfig()
+    mlflow_config = MLFlowConfig(tracking_uri, registry_uri)
     print("Creating New Experiment")
     experiment_id = create_experiment(
         mlflow_config.mlflow_client,
