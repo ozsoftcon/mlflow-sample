4303e112e421d3281b3800a65d70df41b9878716
Rajib Chakravorty <129637958+ozsoftcon@users.noreply.github.com>
2023-07-08 22:09:46 +1000

    Revert "Issues/14 (#19)" (#20)
    This reverts commit b49fc1556783522ce08a630d4e8909580b9e8067.

--- app/main.py
+++ app/main.py
@@ -1,32 +1,25 @@
-from os import environ
 from dotenv import load_dotenv
-from mlflow import log_param, ActiveRun
 from ozsoftcon.mlflow_wrap import (
     MLFlowConfig, create_experiment
 )
 
 load_dotenv()
 
-tracking_uri = environ.get("MLFLOW_TRACKING_URI", "")
-registry_uri = environ.get("MLFLOW_REGISTRY_URI", "")
-
-
 def main():
 
-    mlflow_config = MLFlowConfig(tracking_uri, registry_uri)
+    mlflow_config = MLFlowConfig()
     print("Creating New Experiment")
     experiment_id = create_experiment(
         mlflow_config.mlflow_client,
-        "sample_experiment3"
+        "sample_experiment2"
     )
-
-    current_run = mlflow_config.create_run_for_experiment(
-        experiment_id,
-        tags={"test": "test"},
-        run_name="run_name"
+    print(f"New Experiment ID: {experiment_id}")
+    print("Reusing Old Experiment")
+    experiment_id = create_experiment(
+        mlflow_config.mlflow_client,
+        "sample_experiment2"
     )
-    with ActiveRun(current_run) as run:
-        log_param("test-value", 10)
+    print(f"Reusing Experiment Id {experiment_id}")
 
 
 if __name__ == "__main__":
--- ozsoftcon/mlflow_wrap/mlflow_config.py
+++ ozsoftcon/mlflow_wrap/mlflow_config.py
@@ -1,9 +1,8 @@
-from typing import Optional, Dict, Any
-from uuid import uuid4
+from os import environ
+
 from dotenv import load_dotenv
 from mlflow.client import MlflowClient
 from mlflow import set_tracking_uri, set_registry_uri
-from mlflow.entities import Run
 
 from ..utils import InvalidMLFlowUri
 
@@ -12,11 +11,10 @@ load_dotenv()
 
 class MLFlowConfig():
 
-    def __init__(
-            self,
-            tracking_uri: str = "",
-            registry_uri: str = ""
-    ):
+    def __init__(self):
+
+        tracking_uri = environ.get("MLFLOW_TRACKING_URI", "")
+        registry_uri = environ.get("MLFLOW_REGISTRY_URI", "")
 
         if tracking_uri == "" or registry_uri == "":
             raise InvalidMLFlowUri(
@@ -31,19 +29,3 @@ class MLFlowConfig():
             tracking_uri,
             registry_uri
         )
-
-    def create_run_for_experiment(
-            self,
-            experiment_id: str,
-            tags: Optional[Dict[str, Any]] = None,
-            run_name: Optional[str] = None
-    ) -> Run:
-        if not run_name:
-            run_name = f"Run {uuid4().hex} created for experiment"
-        new_run = self.mlflow_client.create_run(
-            experiment_id, tags=tags, run_name=run_name
-        )
-
-        return new_run
-
-
